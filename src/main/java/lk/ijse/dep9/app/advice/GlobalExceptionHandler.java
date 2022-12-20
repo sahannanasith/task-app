00ec8d754faae21265dc43c49d07da08244dd001
sahannanasith <sahannanasith@gmail.com>
2022-12-20 22:05:02 +0530

    Add validation exception handler

--- src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
+++ src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package lk.ijse.dep9.app.advice;
 
 import lk.ijse.dep9.app.dto.ErrorResponseMsgDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -9,6 +12,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Throwable.class)
     public ErrorResponseMsgDTO uncaughtExceptions(Throwable t){
+        t.printStackTrace();
         return new ErrorResponseMsgDTO(t.getMessage(), 405);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponseMsgDTO validationExceptions(MethodArgumentNotValidException e){
+        String message = e.getFieldErrors().stream().
+                map(err -> err.getField() + ": " + err.getDefaultMessage() + ", ")
+                .reduce((prev, cur) -> prev + cur).get();
+        return new ErrorResponseMsgDTO(message, 400);
     }
 }
--- src/main/java/lk/ijse/dep9/app/api/UserController.java
+++ src/main/java/lk/ijse/dep9/app/api/UserController.java
@@ -2,33 +2,30 @@ package lk.ijse.dep9.app.api;
 
 import lk.ijse.dep9.app.dto.UserDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "application/json")
     public void createUserAccount(@Valid @RequestBody UserDTO user){
         System.out.println(user);
     }
-
     @PatchMapping("/me")
     public void updateUserAccountDetails(){
-
     }
-
     @GetMapping("/me")
     public void getUserAccountDetails(){
-
     }
-
     @DeleteMapping("/me")
     public void deleteUserAccount(){
-
     }
 }
