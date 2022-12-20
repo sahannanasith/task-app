6cb3649885d5b0e3c3b4eb39fc91776efb787aa9
sahannanasith <sahannanasith@gmail.com>
2022-12-20 15:05:06 +0530

    Add ConnectionUtil

---
+++ src/main/java/lk/ijse/dep9/app/dao/util/ConnectionUtil.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep9.app.dao.util;
+
+import java.sql.Connection;
+
+public class ConnectionUtil {
+    private static final ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
+
+    public static void setConnection(Connection connection){
+        threadLocal.set(connection);
+    }
+
+    public static Connection getConnection(){
+        return threadLocal.get();
+    }
+}
+
