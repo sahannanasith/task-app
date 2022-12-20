0f39b9c4dcc005bd2b15c4987b3eaa2410637c55
sahannanasith <sahannanasith@gmail.com>
2022-12-20 18:56:20 +0530

    Add UserDTO

---
+++ src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep9.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO implements Serializable {
+    private String fullName;
+    private String username;
+    private String password;
+}
