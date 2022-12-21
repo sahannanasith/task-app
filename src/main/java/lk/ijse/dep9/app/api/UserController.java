ec0fa80f4c61812778d335e8c8c03d6dd40d99b8
sahannanasith <sahannanasith@gmail.com>
2022-12-21 13:07:50 +0530

    Add SuperService

--- src/main/java/lk/ijse/dep9/app/api/UserController.java
+++ src/main/java/lk/ijse/dep9/app/api/UserController.java
@@ -3,14 +3,10 @@ package lk.ijse.dep9.app.api;
 import lk.ijse.dep9.app.dto.UserDTO;
 import lk.ijse.dep9.app.util.ValidationGroups;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -19,25 +15,25 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "application/json")
-    public void createUserAccount(@Validated(ValidationGroups.Create.class) @RequestBody UserDTO user){
+    public void createUserAccount(@Validated(ValidationGroups.Create.class) @RequestBody UserDTO user) {
         System.out.println(user);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/me", consumes = "application/json")
-    public void updateUserAccountDetails(@Valid @RequestBody UserDTO user){
+    public void updateUserAccountDetails(@Valid @RequestBody UserDTO user) {
         System.out.println(user);
     }
 
     @GetMapping(value = "/me", produces = "application/json")
-    public UserDTO getUserAccountDetails(){
+    public UserDTO getUserAccountDetails() {
         System.out.println("getUserAccountDetails()");
         return new UserDTO();
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/me")
-    public void deleteUserAccount(){
+    public void deleteUserAccount() {
         System.out.println("deleteUserAccount()");
     }
 }
