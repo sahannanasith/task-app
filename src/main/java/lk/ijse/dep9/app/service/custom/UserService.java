e85d39de2fd7f0a729facbfba94c4b07c457f921
sahannanasith <sahannanasith@gmail.com>
2022-12-21 14:17:28 +0530

    Refer the jdbc connection pool inside the web.xml

--- src/main/java/lk/ijse/dep9/app/service/custom/UserService.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/UserService.java
@@ -1,6 +1,10 @@
 package lk.ijse.dep9.app.service.custom;
 
+import lk.ijse.dep9.app.dto.UserDTO;
 import lk.ijse.dep9.app.service.SuperService;
 
 public interface UserService extends SuperService {
+
+    void createNewUserAccount(UserDTO userDTO);
+
 }
--- src/main/java/lk/ijse/dep9/app/service/custom/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/impl/UserServiceImpl.java
@@ -1,6 +1,21 @@
 package lk.ijse.dep9.app.service.custom.impl;
 
+import lk.ijse.dep9.app.dao.DAOFactory;
+import lk.ijse.dep9.app.dao.DAOTypes;
+import lk.ijse.dep9.app.dao.custom.UserDAO;
+import lk.ijse.dep9.app.dto.UserDTO;
 import lk.ijse.dep9.app.service.custom.UserService;
 
 public class UserServiceImpl implements UserService {
+
+    private UserDAO userDAO;
+
+    public UserServiceImpl() {
+        userDAO = DAOFactory.getInstance().getDAO(null, DAOTypes.USER, UserDAO.class);
+    }
+
+    @Override
+    public void createNewUserAccount(UserDTO userDTO) {
+
+    }
 }
