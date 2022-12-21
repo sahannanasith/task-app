dec0186779432f5d0fd1cabe73297723aa542ad8
sahannanasith <sahannanasith@gmail.com>
2022-12-21 13:27:06 +0530

    Finalize the service layer structure

---
+++ src/main/java/lk/ijse/dep9/app/service/ServiceFactory.java
@@ -0,0 +1,27 @@
+package lk.ijse.dep9.app.service;
+
+import lk.ijse.dep9.app.service.custom.impl.ProjectTaskServiceImpl;
+import lk.ijse.dep9.app.service.custom.impl.UserServiceImpl;
+
+public class ServiceFactory {
+
+    private static ServiceFactory serviceFactory;
+
+    private ServiceFactory() {
+    }
+
+    public static ServiceFactory getInstance() {
+        return (serviceFactory == null) ? (serviceFactory = new ServiceFactory()) : serviceFactory;
+    }
+
+    public <T extends SuperService> T getService(ServiceTypes serviceType, Class<T> clazz){
+        switch (serviceType){
+            case USER:
+                return clazz.cast(new UserServiceImpl());
+            case PROJECT_TASK:
+                return clazz.cast(new ProjectTaskServiceImpl());
+            default:
+                return null;
+        }
+    }
+}
---
+++ src/main/java/lk/ijse/dep9/app/service/ServiceTypes.java
@@ -0,0 +1,5 @@
+package lk.ijse.dep9.app.service;
+
+public enum ServiceTypes {
+    USER, PROJECT_TASK
+}
