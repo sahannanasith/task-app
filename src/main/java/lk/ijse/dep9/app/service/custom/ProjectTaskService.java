2de5b9f42e2f9a1364ecb651586b8e6df0181594
sahannanasith <sahannanasith@gmail.com>
2022-12-21 13:13:58 +0530

    Add service facade

---
+++ src/main/java/lk/ijse/dep9/app/service/custom/ProjectTaskService.java
@@ -0,0 +1,6 @@
+package lk.ijse.dep9.app.service.custom;
+
+import lk.ijse.dep9.app.service.SuperService;
+
+public interface ProjectTaskService extends SuperService {
+}
---
+++ src/main/java/lk/ijse/dep9/app/service/custom/UserService.java
@@ -0,0 +1,6 @@
+package lk.ijse.dep9.app.service.custom;
+
+import lk.ijse.dep9.app.service.SuperService;
+
+public interface UserService extends SuperService {
+}
